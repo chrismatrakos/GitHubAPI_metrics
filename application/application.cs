a224b659a82fe323e9bba14978ac7e7dd00d0b47
Christo.Matrakou <christo.matrakou@motorolasolutions.com>
2021-10-29 14:07:00 +0200

    init

--- application/application.cs
+++ application/application.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Application
-{
+namespace Application {
     class Program
     {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Enter access token key!");
-        Console.ReadLine();
-    }
+        static void Main(string[] args) {
+            Console.WriteLine("Enter access token key!");
+            Console.ReadLine();
+        }
     }
 }
