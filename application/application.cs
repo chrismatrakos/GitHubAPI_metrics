d32c5d0b99c13427d584839e537156276a210c46
Christo.Matrakou <chrismatrakou@gmail.com>
2021-10-31 15:18:55 +0100

    add init

--- application/application.cs
+++ application/application.cs
@@ -2,13 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Application {
     class Program
     {
         static void Main(string[] args) {
             Console.WriteLine("Enter access token key!");
+            string input = Console.ReadLine();
+            Console.WriteLine("User entered " + input);
             Console.ReadLine();
         }
     }
