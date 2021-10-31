3a2d0677330f8ef0c4e98653d9a6b86308418020
Christo.Matrakou <chrismatrakou@gmail.com>
2021-11-01 00:36:00 +0100

    add

--- application/application.cs
+++ application/application.cs
@@ -11,6 +11,16 @@ namespace Application {
             string input = Console.ReadLine();
             Console.WriteLine("User entered " + input);
             Console.ReadLine();
+            Console.WriteLine("Making API Call...");
+            using (var client = new HttpClient(new HttpClientHandler { AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate }))
+            {
+                client.BaseAddress = new Uri("https://api.stackexchange.com/2.2/");
+                HttpResponseMessage response = client.GetAsync("answers?order=desc&sort=activity&site=stackoverflow").Result;
+                response.EnsureSuccessStatusCode();
+                string result = response.Content.ReadAsStringAsync().Result;
+                Console.WriteLine("Result: " + result);
+            }
+            Console.ReadLine();
         }
     }
 }
