6dace9c270957b3cc6bc6b705af573ea6c836055
Christo.Matrakou <chrismatrakou@gmail.com>
2021-11-01 21:06:06 +0100

    workign with dynamic object deserialize not Class User deserialize

--- Program.cs
+++ Program.cs
@@ -3,10 +3,11 @@ using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Net.Http;
+using Newtonsoft.Json;
 
 namespace GitHubAPI_metrics {
-    class Program {
-        static void Main(string[] args) {
+    public class Program {
+        public static void Main(string[] args) {
             Console.WriteLine("Enter access token key!");
             // string token = Console.ReadLine();
             var token = "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";
@@ -19,7 +20,7 @@ namespace GitHubAPI_metrics {
 	    public static async Task ExecuteAsync(string token) {
 		    HttpClient client = new HttpClient();
 		    client.BaseAddress = new Uri("https://api.github.com");
-		    client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "2.0"));
+		    client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
 		    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 		    // client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", token);
 		    var response = await client.GetAsync("/rate_limit");
@@ -29,6 +30,11 @@ namespace GitHubAPI_metrics {
             if (response.IsSuccessStatusCode) {
                 var result  = response.Content.ReadAsStringAsync().Result;
                 Console.WriteLine(result);
+                dynamic obj = JsonConvert.DeserializeObject<dynamic>(result);
+				Console.WriteLine("limit: " + obj.rate.limit);
+                Console.WriteLine("remaining: " + obj.rate.remaining);
+				Console.WriteLine("used: " + obj.rate.used);
+
                 // var s = Newtonsoft.Json.JsonConvert.DeserializeObject(result);
                 Console.WriteLine("SUCCESS");
             }
