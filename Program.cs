cccdbd1880a3faed1a5356297dcd2600b1d5952f
Christo.Matrakou <christo.matrakou@motorolasolutions.com>
2021-11-01 17:27:44 +0100

    update

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace GitHubAPI_metrics {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Enter access token key!");
+            // string token = Console.ReadLine();
+            var token = "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";
+            Console.WriteLine("User entered " + token);
+            Console.WriteLine("Making API Call...");
+            Task.WaitAll(ExecuteAsync(token));
+            Console.ReadLine();
+        }
+        
+	    public static async Task ExecuteAsync(string token) {
+		    HttpClient client = new HttpClient();
+		    client.BaseAddress = new Uri("https://api.github.com");
+		    client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
+		    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+		    client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", token);
+		    var response = await client.GetAsync("/rate_limit");
+		    Console.WriteLine("====================================================");
+		    Console.WriteLine(response);
+	    }
+    }
+
+    class User {
+        
+    }
+}
