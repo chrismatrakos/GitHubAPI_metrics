13b22e44fa2481c13646118e74ad0c0be5b153c9
Christo.Matrakou <chrismatrakou@gmail.com>
2021-11-01 01:39:11 +0100

    test

--- application/application.cs
+++ application/application.cs
@@ -5,17 +5,17 @@ using System.Net.Http;
 class Program {
     static void Main(string[] args) {
         Console.WriteLine("Enter access token key!");
-        string input = Console.ReadLine();
-        Console.WriteLine("User entered " + input);
+        // string token = Console.ReadLine();
+        var token = "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";
+        Console.WriteLine("User entered " + token);
         Console.WriteLine("Making API Call...");
+        Task.WaitAll(ExecuteAsyn(token));
         Console.ReadLine();
-        Task.WaitAll(ExecuteAsyn());
     }
         
-	public static async Task ExecuteAsync() {
+	public static async Task ExecuteAsync(string token) {
 		HttpClient client = new HttpClient();
 		client.BaseAddress = new Uri("https://api.github.com");
-		var token = "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";
 		client.DefaultRequestHeaders.UserAgent.Add(new System.Net.Http.Headers.ProductInfoHeaderValue("AppName", "1.0"));
 		client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
 		client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Token", token);
